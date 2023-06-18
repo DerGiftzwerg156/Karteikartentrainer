5661cc1810e1aba0045cb58228d1e8efc5251f58
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2023-06-18 22:25:44 +0200

    Backend-Ready V1.0 Frontend First Steps

---
+++ packages/frontend/src/entitys/CardType.ts
@@ -0,0 +1,11 @@
+export class CardType {
+  id: number|undefined;
+  name: string;
+  description: string;
+
+  constructor(id: number|undefined, name: string, description: string) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
+}
