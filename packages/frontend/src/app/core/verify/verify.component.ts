f17f9b4ea1c710496f25e50f422972fa45ad6455
DerGiftzwerg156 <50719189+DerGiftzwerg156@users.noreply.github.com>
2023-06-22 17:20:57 +0200

    Fixed Verify Method

--- packages/frontend/src/app/core/verify/verify.component.ts
+++ packages/frontend/src/app/core/verify/verify.component.ts
@@ -10,23 +10,28 @@ import {LoggerService} from "../../../services/logger.service";
   styleUrls: ['./verify.component.css']
 })
 export class VerifyComponent implements OnInit {
+  mail: string = "";
+  // @ts-ignore
+  verificationCode: number;
 
-  constructor(private router: Router, private userService: UserService, private route: ActivatedRoute,private logger:LoggerService) {
+  constructor(private router: Router, private userService: UserService, private route: ActivatedRoute, private logger: LoggerService) {
   }
 
   ngOnInit() {
     const routeParams = this.route.snapshot.paramMap;
     const mail = routeParams.get('mail')!;
     const verifyCode = Number(routeParams.get('verifyCode'));
-    this.verify(mail,verifyCode);
+    this.mail = mail;
+    this.verificationCode = verifyCode;
+    // this.verify(mail,verifyCode);
   }
 
-  verify(mail: string, verifyCode: number) {
-    this.userService.verifyAccount(new ActivateAccountRequest(mail, verifyCode)).subscribe(res=>{
-      if(res){
+  verify() {
+    this.userService.verifyAccount(new ActivateAccountRequest(this.mail, this.verificationCode)).subscribe(res => {
+      if (res) {
         this.router.navigate(['']);
         this.logger.showSuccess("Account wurde erfolgreich Verifiziert!")
-      }else {
+      } else {
         this.logger.showError("Es ist ein Fehler aufgetreten, bitte versuchen sie es erneut oder wenden sie sich an den Admin!")
       }
     })
